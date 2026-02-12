954dffa65e1a8a9204e9d81fce432ddb6b1a39d0
dishitkavad <dishitkavad@gmai.com>
2026-02-12 10:08:08 +0530

    set the base of poll application

--- djangotutorial/polls/models.py
+++ djangotutorial/polls/models.py
@@ -1,6 +1,7 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 
 class Question(models.Model):
@@ -8,6 +9,14 @@ class Question(models.Model):
     pub_date = models.DateTimeField("date published")
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
